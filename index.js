ae6412bd474929a2d39dec74e829eca750315245
Johann <johannd0426@gmail.com>
2022-05-02 08:55:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Ship() {
 
+	constructor() {
+	}
+	/*
+	 *
+	*/
+	initialize() {
+		
+		// loads
+		this.load = [65, 75, 100];
+		// heading
+		this.hdg = [20, 200 , 20, 200];
+		// speed over ground
+		this.sog = [];
+		// shaft speed [rpm]
+		this.rpm=[];
+		// shaft power [kW]
+		this.power=[];
+		this.wind=[];
+		this.wave=[];
+
+		
+	}
+
 }
