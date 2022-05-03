f5d59390c4a65a5d2b556c2318b1ce306e4e3a31
Johann <johannd0426@gmail.com>
2022-05-03 11:57:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ class Ship {
 
 	constructor() {
 
-		this.initialize();
 		this.table = document.getElementById( "table1" );
+		this.initialize();
 
 	}
 
 	initialize() {
+
+		const table = this.table;
 
 		this.Za = 52.4;
 		this.Zref = 10;
@@ -24,24 +26,104 @@ class Ship {
 		this.power = [ 12850, 12850, 12850, 12850, 12850, 12850, 12850, 12850 ];
 		this.wind_v = [ 12.43, 12.39, 11.79, 13.15, 11.68, 13.67, 11.77, 13.88 ];
 		this.wind_d = [ 16.4, 346.8, 17.1, 351.3, 16.2, 353.6, 15.0, 355.8 ];
-		this.wave_h = [ 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 ];
-    this.wave_d = [ 0, 180, 0, 180, 0, 180, 0, 180];
-    this.wave_p = [ 4, 4, 4, 4, 4, 4, 4, 4 ];
-    this.swell_h = [ 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 ];
-    this.swell_d = [ 0, 180, 0, 180, 0, 180, 0, 180];
-    this.swell_p = [ 4, 4, 4, 4, 4, 4, 4, 4 ];
-
+		
+    this.wave = [];
+    [ 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5].map( ( e, i ) => this.wave[ i ] = { height: e } );
+    [ 0, 180, 0, 180, 0, 180, 0, 180].map( ( e, i ) => this.wave[ i ].angle = e );
+    [ 4, 4, 4, 4, 4, 4, 4, 4 ].map( ( e, i ) => this.wave[ i ].period = e );
+    
+    this.swell = [];
+    [ 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 ].map( ( e, i ) => this.swell[ i ] = { height: e } );
+    [ 0, 180, 0, 180, 0, 180, 0, 180].map( ( e, i ) => this.swell[ i ].angle = e );
+    [ 8, 8, 8, 8, 8, 8, 8, 8].map( ( e, i ) => this.swell[ i ].period = e );
+   
+   this.hWave = document.getElementById( "hWave" );
+   console.log(hWave.rowIndex)
+   
+   this.wave.map( ( e, i ) => {
+   
+   	document.getElementById( "hWave" ).cells[ i + 1 ].innerHTML = e.height.toFixed(1);
+    document.getElementById( "aWave" ).cells[ i + 1 ].innerHTML = e.angle.toFixed(1);
+    document.getElementById( "pWave" ).cells[ i + 1 ].innerHTML = e.period.toFixed(1);
+   
+   } );
+   
+   this.swell.map( ( e, i ) => {
+   
+   	document.getElementById( "hSwell" ).cells[ i + 1 ].innerHTML = e.height.toFixed(1);
+    document.getElementById( "aSwell" ).cells[ i + 1 ].innerHTML = e.angle.toFixed(1);
+    document.getElementById( "pSwell" ).cells[ i + 1 ].innerHTML = e.period.toFixed(1);
+   
+   } );
+   
 	}
+
+	RAW() {
+  
+  	const table = document.createElement('table');
+    document.body.appendChild( table );
+    const nm1 = this.hdg.length - 1;
+    const header = table.createTHead();
+    header.insertRow(0).insertCell(0);
+    header.rows[0].cells[0].colSpan = nm1 + 2;
+    header.rows[0].cells[0].innerHTML = 'Resistance increase due to waves';
+    header.rows[0].cells[0].style.fontWeight = 'bold';
+    
+    const wave = this.wave;
+    const swell = this.swell;
+    
+    let row1, row2, row3;
+
+		row1 = table.insertRow();
+		row2 = table.insertRow();
+    row3 = table.insertRow();
+		row1.insertCell( -1 ).innerHTML = 'Wave height';
+    row2.insertCell( -1 ).innerHTML = 'Wave angle';
+    row3.insertCell( -1 ).innerHTML = 'Wave period';
+    
+    for ( let i = 0; i <= nm1; i ++ ) {
+    
+    	row1.insertCell( -1 ).innerHTML = wave[ i ].height.toFixed(2);
+      row2.insertCell( -1 ).innerHTML = wave[ i ].angle.toFixed(2);
+      row3.insertCell( -1 ).innerHTML = wave[ i ].period.toFixed(2);
+    
+    }
+    
+    
+    
+  }
 
 	RAA() {
 
-		const table = this.table;
+    const table = document.createElement('table');
+    document.body.appendChild( table );
+    const nm1 = this.hdg.length - 1;
+    const header = table.createTHead();
+    header.insertRow(0).insertCell(0);
+    header.rows[0].cells[0].colSpan = nm1 + 2;
+    header.rows[0].cells[0].innerHTML = 'Resistance increase due to wind';
+    header.rows[0].cells[0].style.fontWeight = 'bold';
+    
 		const ave = true;
-		const nm1 = this.hdg.length - 1;
 		const vwt = [];
 		const dwt = [];
 		let row1, row2, row3;
 
+		row1 = table.insertRow();
+		row2 = table.insertRow();
+		row1.insertCell( -1 ).innerHTML = "Relative wind velocity at anemometer height";
+		row2.insertCell( -1 ).innerHTML = "Relative wind direction at anemometer height";
+    
+    for ( let i = 0; i <= nm1; i ++ ) {
+
+			const vwr = this.wind_v[ i ];
+			const dwr = this.wind_d[ i ];
+
+			row1.insertCell( -1 ).innerHTML = vwr.toFixed(2);
+			row2.insertCell( -1 ).innerHTML = dwr.toFixed(1);
+
+		}
+    
 		row1 = table.insertRow();
 		row2 = table.insertRow();
 		row1.insertCell( -1 ).innerHTML = "True wind velocity at anemometer height";
@@ -90,8 +172,6 @@ class Ship {
 
 		}
 
-		
-
 		row1 = table.insertRow();
 		row2 = table.insertRow();
 		row3 = table.insertRow();
@@ -132,5 +212,6 @@ function init() {
 
 	const ship = new Ship();
 	ship.RAA();
+  ship.RAW();
 
 }
