c459ff492e93aae326f84af0badb6040b1869b9c
Johann <johannd0426@gmail.com>
2024-04-25 17:11:23 +0900

    Update UITable.js

--- UITable.js
+++ UITable.js
@@ -1,30 +1,120 @@
-class UITable { // only number is acceptable
+class UITable { // only number is acceptable in table body
 
-    constructor( n, m ) {
+    constructor( n = 0, m = 0 ) {
 
-        const scope = this;
         this.dom = document.createElement( 'table' );
-        const table = this.dom;
-        this.precision = 2;
+        this.initialize( n, m );
+        const dom = this.dom;
+        const index = [];
+        
+        const onPointerDown = function ( event ) {
+
+            const rowIndex = event.target.parentNode.rowIndex;
+            const cellIndex = event.target.cellIndex;
+            index[ 0 ] = rowIndex;
+            index[ 1 ] = cellIndex;
+            dom.addEventListener( 'pointermove', onPointerMove );
+
+        }
+
+        const onPointerUp = function ( event ) {
+
+            removeClass();
+            dom.removeEventListener( 'pointermove', onPointerMove );
+
+        }
+
+        const onPointerMove = function ( event ) {
+
+            event.preventDefault();
+            const rowIndex = event.target.parentNode.rowIndex;
+            const cellIndex = event.target.cellIndex;
+            const is = rowIndex > index[ 0 ] ? index[ 0 ] : rowIndex;
+            const ie = rowIndex > index[ 0 ] ? rowIndex : index[ 0 ];
+            const js = cellIndex > index[ 1 ] ? index[ 1 ] : cellIndex;
+            const je = cellIndex > index[ 1 ] ? cellIndex : index[ 1 ];
+            removeClass();
+
+            for ( let i = is; i <= ie; i ++ ) {
+
+                const row = dom.rows[ i ];
+
+                for ( let j = js; j <= je; j ++ ) {
+
+                    const cell = row.cells[ j ];
+                    cell.className = "selected";
+
+                }
+
+            }
+
+        }
+
+        const removeClass = function () {
+
+            for ( let i = 0; i < dom.rows.length; i ++ ) {
+
+                const row = dom.rows[ i ];
+
+                for ( let j = 0; j < row.cells.length; j ++ ) {
+
+                    const cell = row.cells[ j ];
+                    cell.classList.remove("selected");
+
+                }
+
+            }
+
+        }
+
+        const onPaste = ( event ) => {
+
+            event.preventDefault();
+            const txt = ( event.clipboardData || window.clipboardData ).getData( "text" );
+            const arr = txt.split( '\r\n' ).map( row => row.split('\t') );
+            const last = arr[ arr.length - 1 ];
+            last.length == 1 && last[ 0 ] == '' ? arr.pop() : null;
+            console.log( arr )
+            const rowIndex = event.target.parentNode.rowIndex;
+            const cellIndex = event.target.cellIndex;
+            this.paste( arr, rowIndex, cellIndex );
+
+        }
+
+        dom.addEventListener( 'pointerdown', onPointerDown );
+        dom.addEventListener( 'pointerup', onPointerUp );
+        dom.addEventListener( 'paste', onPaste );
+
+    }
+
+    initialize( n, m ) {
+
+        const dom = this.dom;
+
+        while( dom.rows.length > 0 ) {
+
+            dom.deleteRow( 0 );
+
+        }
 
         for( let i = 0; i < n; i ++ ) {
 
-            const row = table.insertRow();
+            const row = new UIRowElement();
+            dom.appendChild( row.dom );
 
             for( let j = 0; j < m; j ++ ) {
                 
-                if ( i && j ) {
+                if ( i ) {
 
                     const cell = new UICellElement();
-                    row.appendChild( cell.dom );
+                    row.insertCell();
 
                 } else {
 
-                    const header = document.createElement( 'th' );
-                    row.appendChild( header );
+                    const cell = new UICellElement( 'th' );
+                    row.insertCell();
 
                 }
-                
 
             }
 
@@ -32,11 +122,20 @@ class UITable { // only number is acceptable
 
     }
 
+    insertRow( row, i = - 1 ) {
+
+        const dom = this.dom;
+        row = new UIRowElement();
+        dom.appendChild( row.dom );
+        return row;
+
+    }
+
     paste( arr, imin = 0, jmin = 0 ) {
 
         const tb = this.dom;
-        const n = tb.rows.length;
-        const m = tb.rows[ 0 ].cells.length;
+        const rows = tb.rows;
+        const n = rows.length;
 
         if( Array.isArray( arr ) ) {
 
@@ -44,13 +143,15 @@ class UITable { // only number is acceptable
 
                 const ii = imin + i;
 
-                if( Array.isArray( row ) ) {
+                if( Array.isArray( row ) && ii < n ) {
+
+                    const m = rows[ ii ].cells.length;
 
                     row.map( ( e, j ) => {
 
                         const jj = jmin + j;
 
-                        if( ii < n && jj < m ) {
+                        if( jj < m ) {
 
                             // const number = parseFloat( e )
                             tb.rows[ imin + i ].cells[ jmin + j ].innerHTML = e;
@@ -77,12 +178,81 @@ class UITable { // only number is acceptable
 
 }
 
-class UICellElement {
+class UIRowElement {
 
     constructor() {
 
-        this.dom = document.createElement( 'td' );
-        this.init();
+        this.dom = document.createElement( 'tr' );
+
+    }
+
+    insertCell( i = - 1 ) {
+
+        const dom = this.dom;
+        const cell = new UICellElement();
+        dom.appendChild( cell.dom );
+        
+        return cell;
+
+    }
+
+    insertHeader( i = - 1 ) {
+
+        const dom = this.dom;
+        const cell = new UICellElement( 'th' );
+        dom.appendChild( cell.dom );
+        
+        return cell;
+
+    }
+
+}
+
+class UICellElement {
+
+    constructor( type = 'td' ) {
+
+        this.dom = document.createElement( type );
+        type == 'td' ? this.init() : null;
+
+    }
+
+    // .textContent property has better performance than use .innerHTML to retrieve or write text inside an element since its not parsed as HTML.
+    get textContent() {
+
+        return this.dom.textContent;
+
+    }
+
+    set textContent( txt ) {
+
+        this.dom.textContent = txt;
+
+    }
+
+    /**
+     * @param {int} span
+     */
+    set colSpan( span ) {
+
+        this.dom.colSpan = parseInt( span );
+
+    }
+
+    get decimal() {
+
+        const txt = this.dom.textContent;
+        const value = parseFloat( txt )
+
+        if( typeof value === 'number' ) {
+
+            return txt.includes('.') ? txt.toString().split('.')[1].length : 0;
+
+        } else {
+
+            return 0;
+
+        }
 
     }
 
@@ -90,43 +260,30 @@ class UICellElement {
 
         const scope = this;
         const dom = this.dom;
-        dom.setAttribute( "contenteditable", true );
-        dom.addEventListener( 'keydown', onKeyDown, false );
-        dom.addEventListener( 'focus', onFocus, false );
-        dom.addEventListener( 'blur', onBlur, false );
-        dom.addEventListener( 'paste', onPaste, false );
 
-        function onPaste( event ) {
+        const onDrop = ( event ) => {
 
+            // to prevent unintended item from being drop down to the cell
             event.preventDefault();
-            const arr = ( event.clipboardData || window.clipboardData ).getData( "text" ).split( '\r\n' ).map( e => e.split('\t') );
-            console.log( arr )
-            const row = this.parentNode;
-            const rowIndex = row.rowIndex;
-            const cellIndex = this.cellIndex;
-            scope.paste( arr, rowIndex, cellIndex );
 
         }
 
-		function onFocus() {
+        const onBlur = () => {
+
+            const value = dom.textContent;
+            const number = parseFloat( value );
+			dom.textContent = number.toFixed( this.decimal );
 
 		}
 
-		function onBlur ( event ) {
-
-            const dom = event.currentTarget;
-            const number = parseFloat( dom.innerHTML );
-			dom.innerHTML = number;
-
-		}
-
-		function onKeyDown( event ) {
+        const onKeyDown = ( event ) => {
 
 			event.stopPropagation();
-            const row = this.parentNode;
+            const row = event.target.parentNode;
             const tb = row.parentNode;
-            const cellIndex = this.cellIndex;
             const rowIndex = row.rowIndex;
+            const cellIndex = event.target.cellIndex;
+            
 
 			switch ( event.code ) {
 
@@ -159,49 +316,12 @@ class UICellElement {
 
 		}
 
-    }
-
-    paste( arr, imin = 0, jmin = 0 ) {
-
-        const row = this.dom.parentNode;
-        const tb = row.parentNode;
-        const n = tb.rows.length;
-        const m = tb.rows[ 0 ].cells.length;
-
-        if( Array.isArray( arr ) ) {
-
-            arr.map( ( row, i ) => {
-
-                const ii = imin + i;
-
-                if( Array.isArray( row ) ) {
-
-                    row.map( ( e, j ) => {
-
-                        const jj = jmin + j;
-
-                        if( ii < n && jj < m ) {
-
-                            // const number = parseFloat( e )
-                            tb.rows[ imin + i ].cells[ jmin + j ].innerHTML = e;
-
-                        }
-
-                    } );
-
-                } else {
-
-                    if( ii < n ) {
-                
-                        tb.rows[ imin + i ].cells[ jmin ].innerHTML = row;
-
-                    }
-
-                }
-
-            } )
-
-        }
+        dom.addEventListener( 'drop', onDrop );
+        dom.addEventListener( 'blur', onBlur, false );
+        dom.addEventListener( 'keydown', onKeyDown, false );
+        
+        // editable cell
+        dom.setAttribute( 'contenteditable', true ); 
 
     }
 
