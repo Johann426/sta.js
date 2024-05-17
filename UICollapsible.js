9f75919c975dc5a80b8a79a6e857ce7a5c1ab57e
Johann <johannd0426@gmail.com>
2024-05-17 16:22:01 +0900

    Create UICollapsible.js

---
+++ UICollapsible.js
@@ -0,0 +1,37 @@
+import { UIElement, UIDiv, UIButton } from "./ui.js";
+
+class UICollapsible extends UIDiv {
+
+    constructor( txt ) {
+
+        super();
+        this.button = new UIButton( txt );
+        this.content = new UIDiv();
+        this.initialize()
+
+    }
+
+    initialize() {
+
+        const { button, content } = this;
+
+        const onClick = () => {
+
+            button.dom.classList.toggle("active"); // if active is set remove it, otherwise add it
+            content.dom.style.maxHeight = content.dom.style.maxHeight ? null : content.dom.scrollHeight + "px";
+
+        }
+
+        button.onClick( onClick );
+        button.setClass( 'collapsible' );
+        content.setClass( 'content' );
+        content.setHeight( '300px' );
+
+
+        this.add( button, content );
+
+    }
+
+}
+
+export { UICollapsible };
